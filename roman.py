326b66650f02ca76d4591c40600a5e9a73211d16
Tan <poey.yeex.tan@intel.com>
2016-12-23 11:08:20 +0800

    fixed input 1 digit error

--- roman.py
+++ roman.py
@@ -33,7 +33,6 @@ if user_input.isalpha():
                 rom_num_list.append(b)
             else:
                 pass
-    print rom_num_list
     length = len(rom_num_list)
     total = rom_num_list[0]
     if length > 1:
@@ -53,57 +52,57 @@ elif user_input.isdigit() and int(user_input) < 5000:
         for rom, num in roman_list.iteritems():
             if int(user_input) == num:
                 b = rom
-                rom_num_list.append(b)
     else:
         for i in a:
             rom_num_list.append(i)
-    print rom_num_list
-    num_input = int(''.join(rom_num_list))
-    for i in range(1,1000):
-        if int(num_input) - 1000 >= 0:
-            num_rom_list.append("M")
-            num_input = num_input - 1000
-        elif int(num_input) - 900 >= 0:
-            num_rom_list.append("CM")
-            num_input = num_input - 900
-        elif num_input - 500 >=  0 :
-            num_rom_list.append("D")
-            num_input = num_input - 500 
-        elif int(num_input) - 400 >= 0:
-            num_rom_list.append("CD")
-            num_input = num_input - 400
-        elif num_input - 100 >= 0:
-            num_rom_list.append("C")
-            num_input = num_input - 100
-        elif int(num_input) - 90 >= 0:
-            num_rom_list.append("XC")
-            num_input = num_input - 90
-        elif num_input - 50 >= 0:
-            num_rom_list.append("L")
-            num_input = num_input - 50
-        elif int(num_input) - 40 >= 0:
-            num_rom_list.append("XL")
-            num_input = num_input - 40
-        elif num_input - 10 >= 0:
-            num_rom_list.append("X")
-            num_input = num_input - 10
-        elif int(num_input) - 9 >= 0:
-            num_rom_list.append("IX")
-            num_input = num_input - 9
-        elif num_input - 5 >= 0:
-            num_rom_list.append("V")
-            num_input = num_input - 5
-        elif int(num_input) - 4 >= 0:
-            num_rom_list.append("IV")
-            num_input = num_input - 4
-        elif num_input - 1 >= 0:
-            num_rom_list.append("I")
-            num_input = num_input - 1
-        elif num_input == 0:
-            break
-        else:
-            pass
-    print ''.join(num_rom_list)
-
+    if len(num_rom_list) > 1:
+        num_input = int(''.join(rom_num_list))
+        for i in range(1,1000):
+            if int(num_input) - 1000 >= 0:
+                num_rom_list.append("M")
+                num_input = num_input - 1000
+            elif int(num_input) - 900 >= 0:
+                num_rom_list.append("CM")
+                num_input = num_input - 900
+            elif num_input - 500 >=  0 :
+                num_rom_list.append("D")
+                num_input = num_input - 500 
+            elif int(num_input) - 400 >= 0:
+                num_rom_list.append("CD")
+                num_input = num_input - 400
+            elif num_input - 100 >= 0:
+                num_rom_list.append("C")
+                num_input = num_input - 100
+            elif int(num_input) - 90 >= 0:
+                num_rom_list.append("XC")
+                num_input = num_input - 90
+            elif num_input - 50 >= 0:
+                num_rom_list.append("L")
+                num_input = num_input - 50
+            elif int(num_input) - 40 >= 0:
+                num_rom_list.append("XL")
+                num_input = num_input - 40
+            elif num_input - 10 >= 0:
+                num_rom_list.append("X")
+                num_input = num_input - 10
+            elif int(num_input) - 9 >= 0:
+                num_rom_list.append("IX")
+                num_input = num_input - 9
+            elif num_input - 5 >= 0:
+                num_rom_list.append("V")
+                num_input = num_input - 5
+            elif int(num_input) - 4 >= 0:
+                num_rom_list.append("IV")
+                num_input = num_input - 4
+            elif num_input - 1 >= 0:
+                num_rom_list.append("I")
+                num_input = num_input - 1
+            elif num_input == 0:
+                break
+            else:
+                pass
+        print ''.join(num_rom_list)
+    else:
+        print b
 else:
     print "Invalid input~"
